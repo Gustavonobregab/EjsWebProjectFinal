b88b39fc851a1ba165ade5fab5199901c82d1b32
Gustavonobregab <gustavonobg@gmail.com>
2024-06-26 12:25:13 -0300

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,14 @@
+function createItems() {
+  const numberOfItems = parseInt(document.getElementById('quantity').value)
+  const container = document.getElementById('items-container')
+  container.innerHTML = ''
+
+  for (let i = 0; i < numberOfItems; i++) {
+    const item = document.createElement('div')
+    item.className = 'item'
+    item.innerText = i + 1
+    container.appendChild(item)
+  }
+}
+
+document.getElementById('submitBtn').addEventListener('click', createItems)
